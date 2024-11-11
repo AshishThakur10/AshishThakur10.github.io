a8eb6f267c3f6543595066d55b39c2df3a83693d
Ashish Thakur <sendashishthakur@gmail.com>
2024-11-12 01:18:41 +0530

    upgrade

---
+++ src/app/core/firebase/firebase-config.const.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAhz3IHk_sJ-a-R46INao1XFa-Okrp56vVM1',
+  authDomain: 'portfolio-mane11.firebaseapp.com',
+  projectId: 'portfolio-mane1',
+  storageBucket: 'portfolio-ane1.appspot.com',
+  messagingSenderId: '3060985856221',
+  appId: '1:306098585622:web:f4e7eb0dfc91be7c94f5371',
+  measurementId: 'G-16YK44RZRX1',
+};
---
+++ src/app/features/projects/projects.component.ts
@@ -0,0 +1,35 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss',
+})
+export class ProjectsComponent {
+  public projects = [
+    {
+      title: 'Farland - 2D Platform Game',
+      description:
+        'Developed a 2D platform game using the GODOT game engine with GD Script..',
+      techList: ['GODOT', 'GDScript',],
+      github: 'https://github.com/AshishThakur10/Farland',
+      screenshot: 'https://ibb.co/Ch12Yw5',
+      live: 'https://ashdev.itch.io/farland',
+    },
+    
+  ];
+
+  public visibleProjects = this.projects.slice(0, 3);
+
+  showMoreProjects() {
+    if (this.visibleProjects.length < this.projects.length) {
+      this.visibleProjects = this.projects.slice(
+        0,
+        this.visibleProjects.length + 3
+      );
+    }
+  }
+}
